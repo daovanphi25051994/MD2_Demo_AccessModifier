7f04f28a6f2ff9094e3c83c4bb89a0fe7b36471a
phi <daovanphi25051994@gmail.com>
2020-03-20 20:42:35 +0700

    access modifier

---
+++ src/phi/A.java
@@ -0,0 +1,14 @@
+package phi;
+
+ class A {
+    public int data=40;
+    public void msg(){System.out.println("Hello java");}
+}
+
+class Simple{
+    public static void main(String args[]){
+        A obj=new A();
+        System.out.println(obj.data);
+        obj.msg();
+    }
+}
